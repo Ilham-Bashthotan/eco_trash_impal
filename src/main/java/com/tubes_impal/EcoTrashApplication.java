3d70f4dd7cd7cdc5d5299bf987175852462d9621
Ilham Bashthotan <142276486+Ilham-Bashthotan@users.noreply.github.com>
2025-11-01 18:12:34 +0700

    Initialize EcoTrash application with Spring Boot setup, PostgreSQL configuration, and basic controller for welcome message

---
+++ src/main/java/com/tubes_impal/EcoTrashApplication.java
@@ -0,0 +1,13 @@
+package com.tubes_impal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTrashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubes_impal/eco_trash/EcoTrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes_impal.eco_trash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
